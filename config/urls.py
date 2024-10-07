95aceab8ebbfa8b1815eb5dad6c780d925eb0c67
Alisherbek2001 <alisherbekyuldashev1211@gmail.com>
2024-10-07 19:43:21 +0500

    add media and media settings

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.urls import re_path
 from rest_framework import permissions
@@ -34,5 +36,4 @@ urlpatterns = [
    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
    path('',include("users.urls"))
-   
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
